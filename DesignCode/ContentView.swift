b665280b4370908b0e6240cbe9a101789052617e
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-04-10 14:39:14 +0200

    home-page1(added the some more modifires to fix the container style)

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,25 +9,28 @@ struct ContentView: View {
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
                 .cornerRadius(10.0)
+                .padding()
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle)
                 .fontWeight(.bold)
-            Text("20 sections - 3 hours")
+            Text("20 sections - 3 hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
+                .foregroundColor(.secondary)
             Text("Build an iOS app for iOS 15 with custom layouts,animations and ...")
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
         }
         .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
+        .padding(.vertical, 20)
         .frame(width: nil, height: 350.0)
-        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("Background")/*@END_MENU_TOKEN@*/)
+        .background(Color("Background"))
         .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
-        .shadow(radius: /*@START_MENU_TOKEN@*/20/*@END_MENU_TOKEN@*/)
-
-
-            
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .padding(.horizontal,20)
     }
 }
 
