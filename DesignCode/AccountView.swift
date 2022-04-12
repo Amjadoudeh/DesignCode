e8c7629e7de66f133022a77543f8d7d8b0fa9069
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-04-12 23:36:09 +0200

    WIP user symbol

--- DesignCode/AccountView.swift
+++ DesignCode/AccountView.swift
@@ -11,6 +11,21 @@ struct AccountView: View {
     var body: some View {
         NavigationView {
             List {
+                Image(systemName:
+                        "person.crop.circle.fill.badge.checkmark")
+                    .symbolVariant(.circle.fill)
+                    .font(.system(size: 32))
+                    .symbolRenderingMode(.palette)
+                    .foregroundStyle(.blue, .blue.opacity(0.3))
+                    .padding()
+                    .background(Circle().fill(.ultraThinMaterial))
+                    // will make an intersting shape using modifiers
+                    .background(Image(systemName: "hexagon")
+                                    .symbolVariant(.fill)
+                                    .foregroundColor(.blue)
+                                    .font(.system(size: 200))
+                                    .offset(x: -50, y: -100)
+                    )
                 Section {
                     Text("Settings")
                     Text("Billing")
