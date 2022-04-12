cbaf29c9f6347392eeb2e6b2a6c8ed9a0550aacf
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-04-12 23:46:47 +0200

    WIP user symbol clean and refactor

--- DesignCode/AccountView.swift
+++ DesignCode/AccountView.swift
@@ -11,25 +11,37 @@ struct AccountView: View {
     var body: some View {
         NavigationView {
             List {
-                Image(systemName:
-                        "person.crop.circle.fill.badge.checkmark")
-                    .symbolVariant(.circle.fill)
-                    .font(.system(size: 32))
-                    .symbolRenderingMode(.palette)
-                    .foregroundStyle(.blue, .blue.opacity(0.3))
-                    .padding()
-                    .background(Circle().fill(.ultraThinMaterial))
+                VStack(spacing: 8) {
+                    Image(systemName:
+                            "person.crop.circle.fill.badge.checkmark")
+                        .symbolVariant(.circle.fill)
+                        .font(.system(size: 32))
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.blue, .blue.opacity(0.3))
+                        .padding()
+                        .background(Circle().fill(.ultraThinMaterial))
                     // will make an intersting shape using modifiers
-                    .background(Image(systemName: "hexagon")
-                                    .symbolVariant(.fill)
-                                    .foregroundColor(.blue)
-                                    .font(.system(size: 200))
-                                    .offset(x: -50, y: -100)
-                    )
+                        .background(Image(systemName: "hexagon")
+                                        .symbolVariant(.fill)
+                                        .foregroundColor(.blue)
+                                        .font(.system(size: 200))
+                                        .offset(x: -50, y: -100)
+                        )
+                    Text("Amjad Oudeh")
+                        .font(.title.weight(.semibold))
+                    HStack {
+                        Image(systemName: "location")
+                            .imageScale(.small)
+                        Text("Berlin")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
                 Section {
-                    Text("Settings")
-                    Text("Billing")
-                    Text("Help")
+                    Label("Settings",systemImage: "gear")
+                    Label("Billing",systemImage: "creditcard")
+                    Label("Help",systemImage: "questionmark")
                 }
                 .listRowSeparatorTint(.blue) // Changing the color of the separator
                 .listRowSeparator(.hidden)
