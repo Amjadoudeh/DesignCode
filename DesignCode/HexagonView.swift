a183fc98f52fce3528556cf103016b2c216ce06d
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-04-15 16:48:22 +0200

    added HwxagonView file and started to edit with canvas

--- DesignCode/HexagonView.swift
+++ DesignCode/HexagonView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct HexagonView: View {
     var body: some View {
-    Canvas { context, size in
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Drawing Code@*/ /*@END_MENU_TOKEN@*/
+        Canvas { context, size in
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Drawing Code@*/ /*@END_MENU_TOKEN@*/
+        }
     }
 }
-
 struct HexagonView_Previews: PreviewProvider {
     static var previews: some View {
         HexagonView()
