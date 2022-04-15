7115f44be3d3706ed10dd369294349e980155bc4
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-04-15 16:46:39 +0200

    added HwxagonView file and started to edit with canvas

---
+++ DesignCode/HexagonView.swift
@@ -0,0 +1,21 @@
+//
+//  HexagonView.swift
+//  DesignCode
+//
+//  Created by Amjad Oudeh on 15.04.22.
+//
+
+import SwiftUI
+
+struct HexagonView: View {
+    var body: some View {
+    Canvas { context, size in
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Drawing Code@*/ /*@END_MENU_TOKEN@*/
+    }
+}
+
+struct HexagonView_Previews: PreviewProvider {
+    static var previews: some View {
+        HexagonView()
+    }
+}
