956a8244484601aca8ac072b372fbab06f1dd4a0
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-04-22 13:27:32 +0200

    created an enum - list of const- and injected to the TabBar to show the selected tab

--- DesignCode/TabBar.swift
+++ DesignCode/TabBar.swift
@@ -1,24 +1,31 @@
 import SwiftUI
 
 struct TabBar: View {
+    @State var selectedTab: Tab = .home
     var body: some View {
+       
         ZStack(alignment: .bottom) {
             ContentView()
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
 
             HStack {
                 ForEach(tabItems) { item in
-                    VStack(spacing: 0) {
-                        Image(systemName: item.icon)
-                            .symbolVariant(.fill)
-                            .font(.body.bold())
-                            .frame(width: 44 , height: 30)
-                        Text(item.text)
-                            .font(.caption2)
-                            .lineLimit(1)
+                    Button {
+                        selectedTab = item.tab
+                    } label: {
+                        VStack(spacing: 0) {
+                            Image(systemName: item.icon)
+                                .symbolVariant(.fill)
+                                .font(.body.bold())
+                                .frame(width: 44 , height: 30)
+                            Text(item.text)
+                                .font(.caption2)
+                                .lineLimit(1)
+                        }
+                        // just to fix the size of each
+                        .frame(maxWidth: .infinity)
                     }
-                    // just to fix the size of each
-                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(.secondary)
                 }
             }
             .padding(.horizontal, 8)
