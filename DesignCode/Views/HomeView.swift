8cfa1a2560f414a91dbfc3a2b4a2be608153c96d
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-04-25 21:49:33 +0200

    created a FeaturedItem View and replaced it in the HomeView

--- DesignCode/Views/HomeView.swift
+++ DesignCode/Views/HomeView.swift
@@ -2,48 +2,9 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 8.0) {
-            Spacer()
-            Image("Logo 2")
-                .resizable(resizingMode: .stretch)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 26.0, height: 26.0)
-                .cornerRadius(10.0)
-                .padding(9)
-                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
-                .strokeStyle(cornerRadius: 20)
-            Text("SwiftUI for iOS 15")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundStyle(.linearGradient(colors: [.primary,.primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
-            Text("20 sections - 3 hours".uppercased())
-                .font(.footnote)
-                .fontWeight(.semibold)
-                .foregroundStyle(.secondary)
-            Text("Build an iOS app for iOS 15 with custom layouts,animations and ...")
-                .font(.footnote)
-                .multilineTextAlignment(.leading)
-                .lineLimit(2)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundStyle(.secondary)
+        ScrollView {
+            FeaturedItem()
         }
-        .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
-        .padding(.vertical, 20)
-        .frame(width: nil, height: 350.0)
-        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous)) // Material is to have a glassy view
-        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
-        .strokeStyle()//after creating the extension I can repleace it with my custom modifier .modifier(StrokeStyle())
-        .padding(.horizontal,20)
-        .background(
-            Image("Blob 1")
-                .offset(x: 250, y: -100)) // transform the image
-        .overlay( // so now we 3 layers of background
-            Image("Illustration 5")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 230)
-                .offset(x: 35, y: -80)
-        )
     }
 }
 
