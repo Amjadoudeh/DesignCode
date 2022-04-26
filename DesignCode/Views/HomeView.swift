223357ed7e98be57649ed8e8f3da1ed687586dc3
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-04-26 18:05:10 +0200

    customising a safeArea for on the navBar to have better display for the content

--- DesignCode/Views/HomeView.swift
+++ DesignCode/Views/HomeView.swift
@@ -4,7 +4,11 @@ struct HomeView: View {
     var body: some View {
         ScrollView {
             FeaturedItem()
+            Color.clear.frame(height: 1000)
         }
+        .safeAreaInset(edge: .top, content: {
+            Color.clear.frame(height: 70)
+        })
         .overlay(
            NavigationBar(title: "Featured")
         )
