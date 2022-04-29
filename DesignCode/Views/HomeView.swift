31169da3c0067f481376dd43918866ce1dc64a3c
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-04-29 11:13:55 +0200

    using geomtryReader to calolate the position of the frame - global

--- DesignCode/Views/HomeView.swift
+++ DesignCode/Views/HomeView.swift
@@ -3,9 +3,14 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         ScrollView {
+            GeometryReader{ proxy in
+                Text("\(proxy.frame(in: .global).minY)")
+                
+            }
             FeaturedItem()
 /// the point of adding the clear color with the frame is to show the scroll bar
             Color.clear.frame(height: 1000)
+            
         }
 /// customising a safe area on the top to make a better display for the content
         .safeAreaInset(edge: .top, content: {
