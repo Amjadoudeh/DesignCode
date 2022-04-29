eb84cb284bdc6ce552f3e0bbe09fcb07da326d0f
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-04-29 11:48:09 +0200

    created a PreferenceKeys file for all of them

---
+++ DesignCode/Models/PreferenceKeys.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+struct ScrollPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
