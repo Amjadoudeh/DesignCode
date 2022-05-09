b9e95d2bf47493cd364e3c0b1ad50fc36071f154
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-05-09 23:39:29 +0200

    created an animatableFont modifier to using it for animating the font in the nav bar

---
+++ DesignCode/Styles/AnimatableFontModifier.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+struct AnimatableFontModifier : AnimatableModifier {
+    var size: Double
+    var weight: Font.Weight = .regular
+    var design: Font.Design = .default
+    
+    var animatableDate: Double {
+        get { size }
+        set { size = newValue }
+    }
+    
+    func body(content: Content) -> some View {
+        content.font(.system(size: size, weight: weight, design: design))
+    }
+}
+
+
+extension View {
+    func animatableFont(size: Double, weight: Font.Weight , design: Font.Design) -> some View {
+        self.modifier(AnimatableFontModifier(size: size, weight: weight, design: design))
+    }
+}
