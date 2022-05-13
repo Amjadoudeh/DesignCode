f0d37ad52228c536573008d1572667e1c836f53d
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-05-13 17:11:43 +0200

    Refactor

--- DesignCode/Views/HomeView.swift
+++ DesignCode/Views/HomeView.swift
@@ -5,28 +5,10 @@ struct HomeView: View {
     var body: some View {
         ZStack {
             Color("Background").ignoresSafeArea()
-            
             ScrollView {
-                ///to target the position of the scrollView(starting form after the navBar) I had to use named( ) with coordinateSpace(name: "scroll"), since global targets the full screen and local targets a fram moves with it
-                GeometryReader{ proxy in
-    //                Text("\(proxy.frame(in: .named("scroll")).minY)")
-                    Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
-                }
-                .frame(height: 0)
-                /// adding a frame to get rid of the default hight of the Geometry
-                
+                scrollDetection
                 /// Creating horizental Navigation
-                TabView {
-                    ForEach(courses) { item in
-                        FeaturedItem(course: item)
-                    }
-                }
-                .tabViewStyle(.page(indexDisplayMode: .never))
-                .frame(height: 430)
-                .background(
-                    Image("Blob 1")
-                        .offset(x: 250, y: -100)) // transform the image
-                
+                featured
                 /// the point of adding the clear color with the frame is to show the scroll bar
                 Color.clear.frame(height: 1000)
                 
@@ -47,14 +29,38 @@ struct HomeView: View {
             })
             .overlay(
                 NavigationBar(title: LocalizationKeys.MainView.navtitle, hasScrolled: $hasScrolled)
-                    
-        )
+                
+            )
         }
     }
-}
-
-struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
+    
+    var scrollDetection: some View {
+        ///to target the position of the scrollView(starting form after the navBar) I had to use named( ) with coordinateSpace(name: "scroll"), since global targets the full screen and local targets a fram moves with it
+        GeometryReader{ proxy in
+            ///                Text("\(proxy.frame(in: .named("scroll")).minY)")
+            Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+        }
+        .frame(height: 0)
+        /// adding a frame to get rid of the default hight of the Geometry
+    }
+    
+    var featured: some View {
+        TabView {
+            ForEach(courses) { item in
+                FeaturedItem(course: item)
+            }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .frame(height: 430)
+        .background(
+            Image("Blob 1")
+                .offset(x: 250, y: -100)) // transform the image
+        
+    }
+    
+    struct HomeView_Previews: PreviewProvider {
+        static var previews: some View {
+            HomeView()
+        }
     }
 }
