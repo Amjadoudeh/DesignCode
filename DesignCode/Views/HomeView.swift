631933d63090df1c59dad2a2850eb794bd679355
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-05-14 23:37:52 +0200

    Fix the animation of the NavigationBar

--- DesignCode/Views/HomeView.swift
+++ DesignCode/Views/HomeView.swift
@@ -14,15 +14,6 @@ struct HomeView: View {
                 Color.clear.frame(height: 1000)
             }
             .coordinateSpace(name: "scroll")
-            .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
-                withAnimation(.easeInOut) {
-                    if value < 0  {
-                        hasScrolled = false
-                    } else {
-                        hasScrolled = true
-                    }
-                }
-            })
             /// customising a safe area on the top to make a better display for the content
             .safeAreaInset(edge: .top, content: {
                 Color.clear.frame(height: 70)
@@ -36,10 +27,19 @@ struct HomeView: View {
     var scrollDetection: some View {
 ///to target the position of the scrollView(starting form after the navBar) I had to use named( ) with coordinateSpace(name: "scroll"), since global targets the full screen and local targets a fram moves with it
         GeometryReader{ proxy in
-            //Text("\(proxy.frame(in: .named("scroll")).minY)")
+           // Text("\(proxy.frame(in: .named("scroll")).minY)")
             Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
         }
         .frame(height: 0) /// adding a frame to get rid of the default hight of the Geometry
+        .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
+            withAnimation(.easeInOut) {
+                if value < 0  {
+                    hasScrolled = false
+                } else {
+                    hasScrolled = true
+                }
+            }
+        })
     }
     
     var featured: some View {
