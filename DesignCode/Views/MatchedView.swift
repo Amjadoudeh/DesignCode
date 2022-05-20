27d7ba40f65ab6ff27d651972ea37cf8a4873875
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-05-20 11:22:02 +0200

    Create a new container

--- DesignCode/Views/MatchedView.swift
+++ DesignCode/Views/MatchedView.swift
@@ -42,19 +42,33 @@ struct MatchedView: View {
                 .padding(20)
             } else {
                 ScrollView {
-                    VStack(alignment: .leading, spacing: 12) {
+                    VStack {
                         Spacer()
-                        Text("Bulid an iOS app for iOS 15 with custom layouts, animation and ... ")
-                            .font(.footnote)
-                            .matchedGeometryEffect(id: "text", in: namespace)
-                        Text("20 Sections - 3 hours".uppercased())
-                            .font(.footnote.weight(.semibold))
-                            .matchedGeometryEffect(id: "subtitle", in: namespace)
-                        Text("SHow 1")
-                            .font(.largeTitle.weight(.bold))
-                            .matchedGeometryEffect(id: "title", in: namespace)
-                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
                     }
+                    .frame(maxWidth: .infinity)
+                    .overlay(
+                        VStack (alignment: .leading, spacing: 12) {
+                            Text("Bulid an iOS app for iOS 15 with custom layouts, animation and ... ")
+                                .font(.footnote)
+                                .matchedGeometryEffect(id: "text", in: namespace)
+                            Text("20 Sections - 3 hours".uppercased())
+                                .font(.footnote.weight(.semibold))
+                                .matchedGeometryEffect(id: "subtitle", in: namespace)
+                            Text("SHow 1")
+                                .font(.largeTitle.weight(.bold))
+                                .matchedGeometryEffect(id: "title", in: namespace)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                            .padding(20)
+                            .background(
+                            Rectangle()
+                                .fill(.ultraThinMaterial)
+                                .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                                .matchedGeometryEffect(id: "blur", in: namespace)
+                            )
+                            .offset(y: 100)
+                    )
                     .frame(height: 500)
                     .padding(20)
                     .foregroundStyle(.black)
@@ -73,7 +87,7 @@ struct MatchedView: View {
                     .mask(
                         RoundedRectangle(cornerRadius: 30, style: .continuous)
                             .matchedGeometryEffect(id: "mask", in: namespace)
-                )
+                    )
                 }
             }
         }
