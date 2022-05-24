c0ed7a9909fd2011be88f5fe90f35cf41796f003
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-05-24 10:24:38 +0200

    Divide the matchedView to components

---
+++ DesignCode/Components /CourseItem.swift
@@ -0,0 +1,64 @@
+//
+//  CourseItem.swift
+//  DesignCode
+//
+//  Created by Amjad Oudeh on 24.05.22.
+//
+
+import SwiftUI
+
+struct CourseItem: View {
+    @Namespace var namespace
+    @State var show = false
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            VStack(alignment: .leading, spacing: 12) {
+                Text("SHow 1")
+                    .font(.largeTitle.weight(.bold))
+                    .matchedGeometryEffect(id: "title", in: namespace)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text("20 Sections - 3 hours".uppercased())
+                    .font(.footnote.weight(.semibold))
+                    .matchedGeometryEffect(id: "subtitle", in: namespace)
+                Text("Bulid an iOS app for iOS 15 with custom layouts, animation and ... ")
+                    .font(.footnote)
+                    .matchedGeometryEffect(id: "text", in: namespace)
+            }
+            .padding(20)
+            .background(
+                Rectangle()
+                    .fill(.ultraThinMaterial)
+                    .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                    .blur(radius: 30)
+                    .matchedGeometryEffect(id: "blur", in: namespace)
+            )
+        }
+        .foregroundStyle(.white)
+        .background(
+            Image("Illustration 9")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .matchedGeometryEffect(id: "image", in: namespace)
+        )
+        .background(
+            Image("Background 5")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .matchedGeometryEffect(id: "background", in: namespace)
+        )
+        .mask(
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .matchedGeometryEffect(id: "mask", in: namespace)
+        )
+        .frame(height: 300)
+        .padding(20)
+    }
+}
+
+struct CourseItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseItem()
+    }
+}
--- DesignCode/Views/MatchedView.swift
+++ DesignCode/Views/MatchedView.swift
@@ -7,48 +7,7 @@ struct MatchedView: View {
     var body: some View {
         ZStack {
             if !show {
-                VStack {
-                    Spacer()
-                    VStack(alignment: .leading, spacing: 12) {
-                        Text("SHow 1")
-                            .font(.largeTitle.weight(.bold))
-                            .matchedGeometryEffect(id: "title", in: namespace)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        Text("20 Sections - 3 hours".uppercased())
-                            .font(.footnote.weight(.semibold))
-                            .matchedGeometryEffect(id: "subtitle", in: namespace)
-                        Text("Bulid an iOS app for iOS 15 with custom layouts, animation and ... ")
-                            .font(.footnote)
-                            .matchedGeometryEffect(id: "text", in: namespace)
-                    }
-                    .padding(20)
-                    .background(
-                        Rectangle()
-                            .fill(.ultraThinMaterial)
-                            .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
-                            .blur(radius: 30)
-                            .matchedGeometryEffect(id: "blur", in: namespace)
-                    )
-                }
-                .foregroundStyle(.white)
-                .background(
-                    Image("Illustration 9")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .matchedGeometryEffect(id: "image", in: namespace)
-                )
-                .background(
-                    Image("Background 5")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .matchedGeometryEffect(id: "background", in: namespace)
-                )
-                .mask(
-                    RoundedRectangle(cornerRadius: 30, style: .continuous)
-                        .matchedGeometryEffect(id: "mask", in: namespace)
-                )
-                .frame(height: 300)
-                .padding(20)
+                
             } else {
                 ScrollView {
                     VStack {
