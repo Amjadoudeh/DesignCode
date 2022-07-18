4cc25110072b6dd9e1c81c4395d595295c29b9f2
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-07-18 22:14:44 +0200

    Create a list and make searchable

--- DesignCode/Views/SearchView.swift
+++ DesignCode/Views/SearchView.swift
@@ -1,2 +1,21 @@
 import SwiftUI
 
+struct SearchView: View {
+    @State var text = ""
+    
+    var body: some View {
+        NavigationView {
+            List(0 ..< 5) { item in
+                Text("Hello Amjad")
+            }
+            .searchable(text: $text)
+            .navigationTitle("Search")
+        }
+    }
+}
+
+struct SearchView_Previewer: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
