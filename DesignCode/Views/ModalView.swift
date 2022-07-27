ed2874f73249aa0c69d8a2f28819686fca3c76a5
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-07-27 09:24:43 +0200

    Create a new ModalView - signIn/Up ..

---
+++ DesignCode/Views/ModalView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct ModalView: View {
+    
+    var body: some View {
+        ZStack {
+            SignupView()
+        }
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
