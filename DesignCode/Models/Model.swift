15ee801fc4ad2d630095e1ab87e7c78de1ddef63
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-07-27 09:21:57 +0200

    Add new Modal to the Model - to handel the signIn & signUp cases

--- DesignCode/Models/Model.swift
+++ DesignCode/Models/Model.swift
@@ -4,4 +4,11 @@ import SwiftUI
 class Model: ObservableObject {
     @Published var showDetail: Bool = false
     @Published var selectedCourse: Int = 0
+    @Published var selectedModal: Modal = .signIn
+    
+}
+
+enum Modal: String {
+    case signIn
+    case signUp
 }
