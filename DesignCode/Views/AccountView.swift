6b0f0832f08d78f5b0cfb4f3e2ee86819a2674b7
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-15 15:51:53 +0200

    Style the list of the coins

--- DesignCode/Views/AccountView.swift
+++ DesignCode/Views/AccountView.swift
@@ -131,7 +131,22 @@ struct AccountView: View {
     var coins: some View {
         Section(header: Text("Coins")) {
             ForEach(coinModel.coins) { coin in
-                Text(coin.coin_name)
+                HStack {
+                    AsyncImage(url: URL(string: coin.logo)) { image in
+                        image.resizable()
+                            .aspectRatio(contentMode: .fit)
+                    } placeholder: {
+                         ProgressView()
+                    }
+                    .frame(width: 32, height: 32)
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text(coin.coin_name)
+                        Text(coin.acronym)
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                        
+                    }
+                }
             }
         }
     }
