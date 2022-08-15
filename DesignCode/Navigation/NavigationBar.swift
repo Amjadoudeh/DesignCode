7f041d46db07358410e82265dd586c34762a70c4
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-15 09:40:47 +0200

    Create a AvatarView and fetch the avatar image from external server using AsynImage Api and handel all the cases

--- DesignCode/Navigation/NavigationBar.swift
+++ DesignCode/Navigation/NavigationBar.swift
@@ -45,14 +45,7 @@ struct NavigationBar: View {
                     showModel = true
 
                 } label: {
-                Image("Avatar Default")
-/// changing the order of the modifiers could change the design totally
-                    .resizable()
-                    .frame(width: 26, height: 26)
-                    .cornerRadius(10)
-                    .padding(8)
-                    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
-                    .strokeStyle(cornerRadius: 18)
+                AvatarView()    
                 }
                 .sheet(isPresented: $showAccount) {
                     AccountView()
