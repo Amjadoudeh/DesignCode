391bae6dfccf37ecb82f4835fe33ef5dd995e9c1
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-15 14:34:40 +0200

    Create a CoinModel and fetch the data

---
+++ DesignCode/Models/Coin.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct Coin: Identifiable, Codable {
+    var id: Int
+    var coinName: String
+    var acronym: String
+    var logo: String
+}
+
+class CoinModel: ObservableObject {
+    @Published var coins: [Coin] = []
+    
+    @MainActor
+    func fetchCoins() async {
+        do {
+            let url = URL(string: "https://random-data-api.com/api/crypto_coin/random_crypto_coin?size=20")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            coins = try JSONDecoder().decode([Coin].self, from: data)
+            
+        } catch {
+            print("error")
+        }
+    }
+}
