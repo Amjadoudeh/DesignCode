76ad1adcd6d3b2663163923c2c43d367af2aa3f8
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-15 10:17:47 +0200

    Add the islogged to the AvatarView and change its value

--- DesignCode/Components /AvatarView.swift
+++ DesignCode/Components /AvatarView.swift
@@ -1,19 +1,28 @@
 import SwiftUI
 
 struct AvatarView: View {
+    @AppStorage("isLogged") var isLogged = true
+    
     var body: some View {
-        AsyncImage(url: URL(string: "https://picsum.photos/200"),transaction: Transaction(animation: .easeOut)){
-            phase in
-            switch phase {
-            case .success(let image):
-                image.resizable()
-                    .transition(.scale(scale: 0.5, anchor: .center))
-            case .empty:
-                ProgressView()
-            case .failure(_):
-                Color.gray
-            @unknown default:
-                EmptyView()
+        Group {
+            if isLogged {
+                AsyncImage(url: URL(string: "https://picsum.photos/200"),transaction: Transaction(animation: .easeOut)){
+                    phase in
+                    switch phase {
+                    case .success(let image):
+                        image.resizable()
+                            .transition(.scale(scale: 0.5, anchor: .center))
+                    case .empty:
+                        ProgressView()
+                    case .failure(_):
+                        Color.gray
+                    @unknown default:
+                        EmptyView()
+                    }
+                }
+            } else {
+                Image("Avatar Default")
+                    .resizable()
             }
             
         }
