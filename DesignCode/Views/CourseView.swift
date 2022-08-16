fab1cb8d4a695099cd429153d9b70ac20ff236ee
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-16 14:07:55 +0200

    Use the SectionRow in the CourseView

--- DesignCode/Views/CourseView.swift
+++ DesignCode/Views/CourseView.swift
@@ -127,17 +127,14 @@ struct CourseView: View {
     }
 
     var content: some View {
-        VStack(alignment: .leading, spacing: 30) {
-            Text("SwiftUI is hands-down the best way for designers to take a first step into code. ")
-                .font(.title3).fontWeight(.medium)
-            Text("This course")
-                .font(.title).bold()
-            Text("This course is unlike any other. We care about design and want to make sure that you get better at it in the process. It was written for designers and developers who are passionate about collaborating and building real apps for iOS and macOS. While it's not one codebase for all apps, you learn once and can apply the techniques and controls to all platforms with incredible quality, consistency and performance. It's beginner-friendly, but it's also packed with design tricks and efficient workflows for building great user interfaces and interactions.")
-            Text("This year, SwiftUI got major upgrades from the WWDC 2020. The big news is that thanks to Apple Silicon, Macs will be able to run iOS and iPad apps soon. SwiftUI is the only framework that allows you to build apps for all of Apple's five platforms: iOS, iPadOS, macOS, tvOS and watchOS with the same codebase. New features like the Sidebar, Lazy Grid, Matched Geometry Effect and Xcode 12's visual editing tools will make it easier than ever to build for multiple platforms.")
-            Text("Multiplatform app")
-                .font(.title).bold()
-            Text("For the first time, you can build entire apps using SwiftUI only. In Xcode 12, you can now create multi-platform apps with minimal code changes. SwiftUI will automatically translate the navigation, fonts, forms and controls to its respective platform. For example, a sidebar will look differently on the Mac versus the iPad, while using exactly the same code. Dynamic type will adjust for the appropriate platform language, readability and information density. ")
+        VStack(alignment: .leading) {
+            ForEach(Array(courseSections.enumerated()), id: \.offset) { index, section in
+                if index != 0 { Divider() }
+                SectionRow(section: section)
+            }
         }
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .strokeStyle(cornerRadius: 30)
         .padding(20)
     }
 
