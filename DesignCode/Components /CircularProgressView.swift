7454848083ec55c1edcc6c1e21dbea641d76d7d7
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-16 13:01:25 +0200

    Add animation when the ciruleProgressView appear

--- DesignCode/Components /CircularProgressView.swift
+++ DesignCode/Components /CircularProgressView.swift
@@ -1,12 +1,23 @@
 import SwiftUI
 
 struct CircularProgressView: View {
+    var value: CGFloat = 0.2
+    @State var appear = false
+    
     var body: some View {
         Circle()
-            .trim(from: 0, to: 0.4)
+            .trim(from: 0, to: appear ? value : 0)
             .stroke(style: StrokeStyle(lineWidth: 4, lineCap: .round))
             .fill(.angularGradient(colors: [.purple, .pink, .purple], center: .center, startAngle: .degrees(0), endAngle: .degrees(360)))
             .rotationEffect(.degrees(270))
+            .onAppear {
+                withAnimation(.spring().delay(0.6)) {
+                    appear = true
+                }
+            }
+            .onDisappear {
+                appear = false
+            }
     }
 }
 
