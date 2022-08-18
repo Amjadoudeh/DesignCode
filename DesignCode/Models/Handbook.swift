6f167228d0e05ade577c9c563960ad793289920e
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-18 17:01:11 +0200

    Create a handbookItemView

--- DesignCode/Models/Handbook.swift
+++ DesignCode/Models/Handbook.swift
@@ -15,4 +15,3 @@ var handbooks = [
     Handbook(title: "SwiftUI Handbook", subtitle: "80 sections - 9 hours", text: "A comprehensive series of tutorials covering Xcode, SwiftUI and all the layout and development techniques", logo: "Logo 1", image: "Illustration 1", color1: .teal, color2: .blue),
     Handbook(title: "React Advanced Handbook", subtitle: "30 sections - 3 hours", text: "A comprehensive guide for an overview of the React library and all its basics", logo: "Logo 1", image: "Illustration 2", color1: .purple, color2: .pink)
 ]
-
