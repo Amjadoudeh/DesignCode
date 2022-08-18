dc153fa3d8926c79535a3615d41fef599db0ecd3
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-18 15:57:05 +0200

    Create the exploreView

--- DesignCode/Views/ExploreView.swift
+++ DesignCode/Views/ExploreView.swift
@@ -3,7 +3,13 @@ import SwiftUI
 struct ExploreView: View {
     var body: some View {
         ZStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Color("Background").ignoresSafeArea()
+            ScrollView {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                    .frame(maxWidth: .infinity)
+            }
+            .overlay(NavigationBar(title: "Recent", hasScrolled: .constant(true)))
+            .background(Image("Blob 1").offset(x: -100, y: -350))
         }
     }
 }
