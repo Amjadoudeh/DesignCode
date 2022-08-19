5c249e23771a07685758ed1870fe3da12f345c21
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-19 12:38:54 +0200

    Create a dataModel for Topic

---
+++ DesignCode/Models/Topic.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct Topic: Identifiable {
+    let id = UUID()
+    var title: String
+    var icon: String
+}
+
+var topics = [
+    Topic(title: "iOS Development", icon: "iphone"),
+    Topic(title: "UI Design", icon: "eyedropper"),
+    Topic(title: "Web development", icon: "laptopcomputer")
+]
