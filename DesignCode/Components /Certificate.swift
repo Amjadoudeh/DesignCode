d44e8c058cc7e50ef770461abc365f2a9846a420
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-20 13:57:10 +0200

    Create the Certificate component

---
+++ DesignCode/Components /Certificate.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct Certificate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Certificate_Previews: PreviewProvider {
+    static var previews: some View {
+        Certificate()
+    }
+}
