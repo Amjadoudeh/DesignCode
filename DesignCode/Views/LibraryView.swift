3529099d89262c277f6c579198ae430961692e00
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-20 13:55:51 +0200

    Copy the style from the ExploreView and edit it

--- DesignCode/Views/LibraryView.swift
+++ DesignCode/Views/LibraryView.swift
@@ -2,7 +2,50 @@ import SwiftUI
 
 struct LibraryView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("Background").ignoresSafeArea()
+            ScrollView {
+                coursesSection
+                
+                Text("Topics".uppercased())
+                    .titleStyle()
+                topicsSection
+                
+                Text("Certificates".uppercased())
+                    .titleStyle()
+                
+            }
+            .safeAreaInset(edge: .top) {
+                Color.clear.frame(height: 70)
+            }
+            .overlay(NavigationBar(title: "Recent", hasScrolled: .constant(true)))
+            .background(Image("Blob 1").offset(x: -100, y: -350))
+        }
+    }
+    var coursesSection: some View {
+        ScrollView(.horizontal,showsIndicators: false) {
+            HStack (spacing: 16){
+                ForEach(courses) { course in
+                    SmallCourseItem(course: course)
+                }
+            }
+            .padding(.horizontal, 20)
+            Spacer()
+            
+        }
+    }
+    
+    
+    var topicsSection: some View {
+        VStack {
+            ForEach(topics) { topic in
+                TopicListRow(topic: topic)
+            }
+        }
+        .padding(20)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .strokeStyle(cornerRadius: 30)
+        .padding(.horizontal, 20)
     }
 }
 
