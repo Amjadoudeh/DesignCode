93a952a9f47128dc091b41884647c85d72aaa14e
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-22 11:11:20 +0200

    Fix the NavBar animation

--- DesignCode/Views/Main/ExploreView.swift
+++ DesignCode/Views/Main/ExploreView.swift
@@ -1,10 +1,12 @@
 import SwiftUI
 
 struct ExploreView: View {
+    @State var hasScrolled: Bool = false
     var body: some View {
         ZStack {
             Color("Background").ignoresSafeArea()
             ScrollView {
+                scrollDetection
                 coursesSection
                 
                 Text("Topics".uppercased())
@@ -19,7 +21,7 @@ struct ExploreView: View {
             .safeAreaInset(edge: .top) {
                 Color.clear.frame(height: 70)
             }
-            .overlay(NavigationBar(title: "Recent", hasScrolled: .constant(false)))
+            .overlay(NavigationBar(title: "Recent", hasScrolled: $hasScrolled))
             .background(Image("Blob 1").offset(x: -100, y: -350))
         }
     }
@@ -55,6 +57,24 @@ struct ExploreView: View {
         .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
         .strokeStyle(cornerRadius: 30)
         .padding(.horizontal, 20)
+    }
+    
+    var scrollDetection: some View {
+        /// to target the position of the scrollView(starting form after the navBar) I had to use named( ) with coordinateSpace(name: "scroll"), since global targets the full screen and local targets a fram moves with it
+        GeometryReader { proxy in
+            // Text("\(proxy.frame(in: .named("scroll")).minY)")
+            Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+        }
+        .frame(height: 0) /// adding a frame to get rid of the default hight of the Geometry
+        .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
+            withAnimation(.easeInOut) {
+                if value < 0 {
+                    hasScrolled = false
+                } else {
+                    hasScrolled = true
+                }
+            }
+        })
     }
 }
 
--- DesignCode/Views/Main/NotificationsView.swift
+++ DesignCode/Views/Main/NotificationsView.swift
@@ -1,18 +1,20 @@
 import SwiftUI
 
 struct NotificationsView: View {
+    @State var hasScrolled: Bool = false
     var body: some View {
         ZStack {
             Color("Background").ignoresSafeArea()
             
             ScrollView {
+                scrollDetection
                 
                 sectionSections
             }
             .safeAreaInset(edge: .top, content: {
                 Color.clear.frame(height: 70)
             })
-            .overlay(NavigationBar(title: "Notifications", hasScrolled: .constant(false)))
+            .overlay(NavigationBar(title: "Notifications", hasScrolled: $hasScrolled))
             .background(Image("Blob 1").offset(x: -180, y: -300))
             
         }
@@ -30,6 +32,23 @@ struct NotificationsView: View {
         .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
         .strokeStyle(cornerRadius: 30)
         .padding(20)
+    }
+    var scrollDetection: some View {
+        /// to target the position of the scrollView(starting form after the navBar) I had to use named( ) with coordinateSpace(name: "scroll"), since global targets the full screen and local targets a fram moves with it
+        GeometryReader { proxy in
+            // Text("\(proxy.frame(in: .named("scroll")).minY)")
+            Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+        }
+        .frame(height: 0) /// adding a frame to get rid of the default hight of the Geometry
+        .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
+            withAnimation(.easeInOut) {
+                if value < 0 {
+                    hasScrolled = false
+                } else {
+                    hasScrolled = true
+                }
+            }
+        })
     }
 }
 
